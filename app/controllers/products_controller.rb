557e68910ab5c3d920960a2096e00d0becbf2845
thejoshlamb <joshlamb@gmail.com>
2013-11-14 18:40:57 -0500

    added pagination controls to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,10 +3,15 @@ class ProductsController < ApplicationController
 	
 
   def index
-  	@products = JSON.parse(open("http://lcboapi.com/products?where=has_limited_time_offer&order=limited_time_offer_savings_in_cents.desc&per_page=10").read) #converts into hash
+  	page = params[:page] || 1
+  	@products = JSON.parse(open("http://lcboapi.com/products?where=has_limited_time_offer&order=limited_time_offer_savings_in_cents.desc&per_page=10&page=#{page}").read)
   end
 
   def show
   	@product = JSON.parse(open("http://lcboapi.com/products/#{params[:id]}").read)['result']
+  end
+
+  def update
+  
   end
 end
