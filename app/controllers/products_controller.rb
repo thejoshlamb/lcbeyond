1ece536ced0ca527f3a3bb7eb6ec17f8d71c2180
thejoshlamb <joshlamb@gmail.com>
2013-11-15 15:19:56 -0500

    setting up heroku

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,9 @@
 class ProductsController < ApplicationController
 
-	
-
   def index
   	page = params[:page] || 1
   	@products = JSON.parse(open("http://lcboapi.com/products?where=has_limited_time_offer&order=limited_time_offer_savings_in_cents.desc&per_page=10&page=#{page}").read)
+  	@clearance = JSON.parse(open("http://lcboapi.com/products?where=has_clearance_sale&order=clearance_sale_savings_in_cents.desc&per_page=30&page=#{page}").read)
   end
 
   def show
